46b223b7ca55612932219cd737c9cc45f0938a38
Brendan Jessen <jes20003@byui.edu>
2021-10-06 11:29:41 -0600

    Updates to player class

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,1 +1,12 @@
 #Player class to do things with the player
+
+
+
+class Player():
+    def __init__(self):
+        self.points = 300
+        self.guess = True
+        self.play_again = True
+
+    def change_points(points):
+        pass
