75c4e376a63e0c8ac7c6f4fa67d3167287762b5a
Brendan Jessen <jes20003@byui.edu>
2021-10-07 15:10:33 -0600

    Just did a quick fix

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,11 +1,10 @@
 #Player class to do things with the player
-#from game.dealer import Dealer
+
 
 class Player():
     def __init__(self):
         self.points = 300
         self.guess = True
-        #self.dealer = Dealer()
 
     def player_move(self):
         guess = input("Higher or lower: [h/l]").lower()
